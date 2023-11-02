f79c2579dbc202a5616d28301252d86718a8a798
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2023-11-02 20:21:28 +0100

    update

--- src/game/state.ts
+++ src/game/state.ts
@@ -1,9 +1,8 @@
 import { create } from 'zustand'
 import { GameState } from './GameState'
 import { InitialGameState } from './InitialGameState'
-import { devtools } from 'zustand/middleware'
-export const useGameStore = create<GameState, [['zustand/devtools', never]]>(devtools(() => InitialGameState))
-// export const useGameStore = create<GameState, []>(() => InitialGameState)
+
+export const useGameStore = create<GameState>()(() => InitialGameState)
 
 if (!('indexedDB' in window)) {
     console.log("This browser doesn't support IndexedDB")
