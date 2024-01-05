ed2de9276c7ef700f8d8eb1942fc3a6e6ca8f621
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2024-01-05 14:22:48 +0100

    getUniqueId

--- src/utils/getUniqueId.ts
+++ src/utils/getUniqueId.ts
@@ -1,7 +1,5 @@
 let lastCounter = 0
-const start = new Date(2023).getTime()
+const start = new Date(2024, 1).getTime()
 export function getUniqueId(): string {
-    lastCounter++
-    const ret = `${(Date.now() - start).toString(36)}-${lastCounter.toString(36)}`
-    return ret
+    return `${(lastCounter++).toString(36)}${(Date.now() - start).toString(36)}`
 }
