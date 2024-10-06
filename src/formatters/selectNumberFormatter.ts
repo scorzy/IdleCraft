f762500cfb04a159858c8f5286e656ce2c1d7137
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2024-10-06 15:44:49 +0200

    numberFormatter

--- src/formatters/selectNumberFormatter.ts
+++ src/formatters/selectNumberFormatter.ts
@@ -1,45 +1,22 @@
-import { default as microMemoize } from 'micro-memoize'
+import { memoize } from 'proxy-memoize'
 import { messages } from '../msg/allMsg'
 import { useGameStore } from '../game/state'
+import { UiState } from '../ui/UiState'
 import { GameState } from '../game/GameState'
 import { getFormatter } from './formatNumber'
 import { getTimeFormatter } from './formatTime'
-import { NotationTypes } from './NotationTypes'
-import { CommaTypes } from './CommaTypes'
 
-const makeFormatter = microMemoize(
-    (comma: CommaTypes, lang: string, notation: NotationTypes) => {
-        const formatter = getFormatter(notation, comma)
-        const t = messages[lang]
-        if (!t) throw new Error(`Language ${lang} not found`)
-        return {
-            f: formatter[0],
-            p: formatter[1],
-            ft: getTimeFormatter(formatter[0], t.fun),
-        }
-    },
-    {
-        maxSize: 1,
+const makeFormatter = memoize((ui: UiState) => {
+    const formatter = getFormatter(ui.numberFormatNotation, ui.comma)
+    const t = messages[ui.lang]
+    if (!t) throw new Error(`Language ${ui.lang} not found`)
+    return {
+        f: formatter[0],
+        p: formatter[1],
+        ft: getTimeFormatter(formatter[0], t.fun),
     }
-)
+})
 
-const makeFormatterFromUi = microMemoize(
-    (comma: CommaTypes, lang: string, notation: NotationTypes) => {
-        return makeFormatter(comma, lang, notation)
-    },
-    {
-        maxSize: 1,
-    }
-)
+const selectFormatter = (s: GameState) => makeFormatter(s.ui)
 
-const selectComma = (s: GameState) => s.ui.comma
-const selectLang = (s: GameState) => s.ui.lang
-const selectNotation = (s: GameState) => s.ui.numberFormatNotation
-
-export const useNumberFormatter = () => {
-    const comma = useGameStore(selectComma)
-    const lang = useGameStore(selectLang)
-    const notation = useGameStore(selectNotation)
-
-    return makeFormatterFromUi(comma, lang, notation)
-}
+export const useNumberFormatter = () => useGameStore(selectFormatter)
--- src/storage/getItemId2.ts
+++
@@ -1,2 +0,0 @@
-export const getItemId2 = (stdItemId: string | null | undefined, craftItemId: string | null | undefined) =>
-    stdItemId ? `s${stdItemId}` : craftItemId ? `c${craftItemId}` : ''
