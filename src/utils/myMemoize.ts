0e5343418b82f7d31bb605d2e215e64363425548
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2024-10-10 21:34:48 +0200

    performances

--- src/utils/myMemoize.ts
+++ src/utils/myMemoize.ts
@@ -3,7 +3,6 @@ import moize from 'moize'
 const options = {
     isDeepEqual: true,
     maxSize: 100,
-    maxAge: 1000 * 60 * 5, // five minutes;
 }
 
 export const myMemoize = moize(options)
--- src/utils/myMemoizeOne.ts
+++ src/utils/myMemoizeOne.ts
@@ -3,7 +3,6 @@ import moize from 'moize'
 const options = {
     isDeepEqual: true,
     maxSize: 1,
-    maxAge: 1000 * 60 * 5, // five minutes;
 }
 
 export const myMemoizeOne = moize(options)
