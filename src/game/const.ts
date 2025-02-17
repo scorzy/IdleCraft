0a006882fe708fcfe78f8922f4c04fb2e0e0cff2
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-02-17 22:24:47 +0100

    refactor: remove mutable functions and streamline adapter functions

--- src/game/const.ts
+++ src/game/const.ts
@@ -1,3 +1,3 @@
 export const MAX_LOAD = 3600 * 1000 * 24 * 7
-//export const TEST_DIF = -3600 * 1000 * 24 * 2
-export const TEST_DIF: number = 0
+export const TEST_DIF = -3600 * 1000 * 24 * 2
+//export const TEST_DIF: number = 0
--- src/game/functions/loadGame.ts
+++ src/game/functions/loadGame.ts
@@ -1,7 +1,6 @@
 import { onTimer } from '../../timers/onTimer'
 import { execTimer } from '../../timers/timerFunctions'
 import { getFirstTimer } from '../../timers/getFirstTimer'
-import { adapterFunctions } from '../../entityAdapter/AdapterFunctions'
 import { GameState } from '../GameState'
 import { regenerate } from '../regenerate'
 import { TEST_DIF, MAX_LOAD } from '../const'
@@ -11,48 +10,44 @@ import { advanceTimers } from './advanceTimers'
 declare function setTimeout(this: Window | void, handler: (...args: unknown[]) => void, timeout: number): number
 
 export function loadGame(state: GameState): GameState {
-    adapterFunctions.setMutable()
-    try {
-        if (TEST_DIF !== 0) {
-            state = advanceTimers(state, TEST_DIF)
-            state.lastRegen += TEST_DIF
-        }
-
-        state.loading = true
-        let diff = Date.now() - state.now - MAX_LOAD
-        if (diff > 0) state = advanceTimers(state, diff)
-
-        const now = Math.min(state.now + MAX_LOAD, Date.now())
-
-        for (const id of state.timers.ids) {
-            const timer = state.timers.entries[id]
-            if (timer) timer.intervalId = undefined
-        }
-
-        let timer = getFirstTimer(state.timers, now)
-        while (timer) {
-            state = onTimer(state, timer.id)
-            state.now = timer.to
-            timer = getFirstTimer(state.timers, now)
-        }
-
-        for (const id of state.timers.ids) {
-            const timer = state.timers.entries[id]
-            if (!timer) continue
-            if (timer.intervalId && timer.intervalId > 0) continue
-
-            const diff = Math.max(timer.to - state.now, 0)
-            timer.intervalId = setTimeout(() => execTimer(id), diff)
-        }
-
-        diff = Date.now() - state.now
-        if (diff > 0) state = advanceTimers(state, diff)
-
-        state = regenerate(state, state.now)
-
-        state.loading = false
-    } finally {
-        adapterFunctions.setImmutable()
+    if (TEST_DIF !== 0) {
+        state = advanceTimers(state, TEST_DIF)
+        state.lastRegen += TEST_DIF
     }
+
+    state.loading = true
+    let diff = Date.now() - state.now - MAX_LOAD
+    if (diff > 0) state = advanceTimers(state, diff)
+
+    const now = Math.min(state.now + MAX_LOAD, Date.now())
+
+    for (const id of state.timers.ids) {
+        const timer = state.timers.entries[id]
+        if (timer) timer.intervalId = undefined
+    }
+
+    let timer = getFirstTimer(state.timers, now)
+    while (timer) {
+        state = onTimer(state, timer.id)
+        state.now = timer.to
+        timer = getFirstTimer(state.timers, now)
+    }
+
+    for (const id of state.timers.ids) {
+        const timer = state.timers.entries[id]
+        if (!timer) continue
+        if (timer.intervalId && timer.intervalId > 0) continue
+
+        const diff = Math.max(timer.to - state.now, 0)
+        timer.intervalId = setTimeout(() => execTimer(id), diff)
+    }
+
+    diff = Date.now() - state.now
+    if (diff > 0) state = advanceTimers(state, diff)
+
+    state = regenerate(state, state.now)
+
+    state.loading = false
+
     return structuredClone(state)
 }
