2875729846410f25fc053ce4466f85fa97ca793f
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-02-24 22:00:34 +0100

    feat: enhance error messages in immutable functions for better debugging

--- src/entityAdapter/AdapterFunctions.ts
+++ src/entityAdapter/AdapterFunctions.ts
@@ -4,27 +4,27 @@ export function createImmutable<T>(state: InitialState<T>, id: string, data: T):
     return { entries: { ...state.entries, [id]: data }, ids: [...state.ids, id] }
 }
 
-export function removeImmutable<T>(state: InitialState<T>, id: string) {
+export function removeImmutable<T>(state: InitialState<T>, id: string): InitialState<T> {
     const { [id]: _, ...newEntries } = state.entries
     return { entries: newEntries, ids: state.ids.filter((e) => e !== id) }
 }
 
-export function updateImmutable<T>(state: InitialState<T>, id: string, data: Partial<T>) {
+export function updateImmutable<T>(state: InitialState<T>, id: string, data: Partial<T>): InitialState<T> {
     const existing = state.entries[id]
-    if (!existing) throw new Error(`${id} doesn't exists`)
+    if (!existing) throw new Error(`[updateImmutable] ${id} doesn't exists`)
     return { ids: state.ids, entries: { ...state.entries, [id]: { ...existing, ...data } } }
 }
 
-export function replaceImmutable<T>(state: InitialState<T>, id: string, data: T) {
+export function replaceImmutable<T>(state: InitialState<T>, id: string, data: T): InitialState<T> {
     const existing = state.entries[id]
-    if (!existing) throw new Error(`${id} doesn't exists`)
-    return { entries: { ...state.entries, [id]: data } }
+    if (!existing) throw new Error(`[replaceImmutable] ${id} doesn't exists`)
+    return { ids: state.ids, entries: { ...state.entries, [id]: data } }
 }
-
 export class AdapterFunctions {
     create = createImmutable
     remove = removeImmutable
     update = updateImmutable
     replace = replaceImmutable
 }
+
 export const adapterFunctions = new AdapterFunctions()
