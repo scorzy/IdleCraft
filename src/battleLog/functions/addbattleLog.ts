504aca26cceea118c7b32261eb3eaf95338baef8
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-04-27 16:17:03 +0200

    refactor: streamline battle log management by adjusting max logs logic

--- src/battleLog/functions/addbattleLog.ts
+++ src/battleLog/functions/addbattleLog.ts
@@ -4,13 +4,16 @@ import { BattleLogAdapter } from '../battleLogAdapter'
 import { AddBattleLog, BattleLog } from '../battleLogInterfaces'
 
 const MAX_LOGS = 100
+const MAX_LOGS_LOADING = 20
 
 export function addBattleLog(state: GameState, addLog: AddBattleLog) {
     const battleLog: BattleLog = { ...addLog, id: getUniqueId(), date: state.now }
 
     let battleLogs = BattleLogAdapter.create(state.battleLogs, battleLog)
 
-    if (battleLogs.ids.length > MAX_LOGS) {
+    const maxLogs = state.loading ? MAX_LOGS_LOADING : MAX_LOGS
+
+    while (battleLogs.ids.length > maxLogs) {
         const id = battleLogs.ids[0]
         if (id) battleLogs = BattleLogAdapter.remove(battleLogs, id)
     }
