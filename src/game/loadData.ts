b658fb6713fb0434f3154b2bb17726dd00eb89fc
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-09-16 22:04:32 +0200

    load with deepmerge

--- src/game/loadData.ts
+++ src/game/loadData.ts
@@ -1,47 +1,9 @@
-import { ActivityAdapter } from '../activities/ActivityState'
-import { ItemAdapter } from '../storage/ItemAdapter'
-import { TimerAdapter } from '../timers/Timer'
-import { copyValues } from '../utils/copyValues'
-import { TreeGrowthAdapter } from '../wood/forest/forestGrowth'
-import { CastCharAbilityAdapter } from '../activeAbilities/abilityAdapters'
-import { CharacterAdapter } from '../characters/characterAdapter'
-import { RecipeParameter, RecipeParameterValue, RecipeResult } from '../crafting/RecipeInterfaces'
-import { loadUi } from '../ui/loadUi'
-import { loadLocation } from '../gameLocations/loadLocations'
-import { QuestAdapter } from '../quests/QuestTypes'
+import { deepmerge } from 'deepmerge-ts'
 import { GameState } from './GameState'
 import { GetInitialGameState } from './InitialGameState'
 
 export function loadData(data: object): GameState {
-    const state = GetInitialGameState()
-    copyValues(state, data)
-    loadUi(data, state)
-
-    if ('activities' in data) state.activities = ActivityAdapter.load(data.activities)
-    if ('timers' in data) state.timers = TimerAdapter.load(data.timers)
-    if ('craftedItems' in data) state.craftedItems = ItemAdapter.load(data.craftedItems)
-    if ('treeGrowth' in data) state.treeGrowth = TreeGrowthAdapter.load(data.treeGrowth)
-    if ('castCharAbility' in data) state.castCharAbility = CastCharAbilityAdapter.load(data.castCharAbility)
-
-    if (
-        'orderedActivities' in data &&
-        data.orderedActivities &&
-        Array.isArray(data.orderedActivities) &&
-        data.orderedActivities.every((e) => typeof e === 'string')
-    )
-        state.orderedActivities = data.orderedActivities
-
-    loadLocation(state, data)
-
-    if ('characters' in data) state.characters = CharacterAdapter.load(data.characters)
-    if ('quests' in data) state.quests = QuestAdapter.load(data.quests)
-
-    if ('craftingForm' in data)
-        state.craftingForm = data.craftingForm as {
-            params: RecipeParameter[]
-            paramsValue: RecipeParameterValue[]
-            result: RecipeResult | undefined
-        }
+    const state = deepmerge(GetInitialGameState(), data) as GameState
 
     return state
 }
