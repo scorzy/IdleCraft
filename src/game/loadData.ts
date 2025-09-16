965ed48f38af70c80badfad1b745b1b596a0be05
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-09-16 22:25:23 +0200

    fix load

--- src/game/loadData.ts
+++ src/game/loadData.ts
@@ -1,9 +1,16 @@
 import { deepmerge } from 'deepmerge-ts'
+import { CharacterAdapter } from '../characters/characterAdapter'
+import { PLAYER_CHAR, PLAYER_ID } from '../characters/charactersConst'
 import { GameState } from './GameState'
 import { GetInitialGameState } from './InitialGameState'
 
 export function loadData(data: object): GameState {
-    const state = deepmerge(GetInitialGameState(), data) as GameState
+    //ToDo: check, don't duplicate ids on array
+
+    const initial = GetInitialGameState()
+    initial.characters = CharacterAdapter.getInitialState()
+    const state = deepmerge(initial, data) as GameState
+    if (!CharacterAdapter.select(state.characters, PLAYER_ID)) CharacterAdapter.create(state.characters, PLAYER_CHAR)
 
     return state
 }
