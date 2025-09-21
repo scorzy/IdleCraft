147a7a75da9628d1fd0598c524d19623d1b97873
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-09-21 12:15:40 +0200

    viewCrafting

--- src/crafting/functions/viewCrafting.ts
+++ src/crafting/functions/viewCrafting.ts
@@ -11,8 +11,8 @@ export const viewCrafting = (state: GameState, activity: ActivityState) => {
     const recipe = recipes.getEx(activity.recipeId)
     state.ui.recipeType = recipe.type
     state.recipeId = recipe.id
-    state.craftingForm.paramsValue = activity.paramsValue
-    state.craftingForm.result = activity.result
+    state.craftingForm.paramsValue = structuredClone(activity.paramsValue)
+    state.craftingForm.result = structuredClone(activity.result)
 
     const page = Object.entries(UiPagesData).find((kv) => kv[1].recipeType && kv[1].recipeType === recipe.type)?.[0]
     if (page) state.ui.page = page as UiPages
