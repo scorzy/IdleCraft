69477d81efe5680395cd2a1487d68bbff47f3a2d
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2025-09-22 21:58:53 +0200

    wood types

--- src/wood/WoodTypes.ts
+++ src/wood/WoodTypes.ts
@@ -1,6 +1,14 @@
 export enum WoodTypes {
     DeadTree = 'DeadTree',
     Oak = 'Oak',
+    Pine = 'Pine',
+    Birch = 'Birch',
+    Maple = 'Maple',
+    Willow = 'Willow',
+    Cedar = 'Cedar',
+    Redwood = 'Redwood',
+    Spruce = 'Spruce',
+    Mahogany = 'Mahogany',
 }
 
 export const WoodTypesString = Object.values(WoodTypes)
