1ed6566ea97b4ea6086eb237b0002bf9e015d12c
Lorenzo Scorzato <scorzy@users.noreply.github.com>
2026-02-04 21:18:44 +0100

    remove  alchemy items from loading

--- src/game/loadData.ts
+++ src/game/loadData.ts
@@ -1,8 +1,6 @@
 import { deepmerge } from 'deepmerge-ts'
 import { CharacterAdapter } from '../characters/characterAdapter'
 import { PLAYER_CHAR, PLAYER_ID } from '../characters/charactersConst'
-import { AlchemyItems } from '../alchemy/alchemyItems'
-import { addItem } from '../storage/storageFunctions'
 import { GameState } from './GameState'
 import { GetInitialGameState } from './InitialGameState'
 
@@ -13,12 +11,6 @@ export function loadData(data: object): GameState {
     initial.characters = CharacterAdapter.getInitialState()
     const state = deepmerge(initial, data) as GameState
     if (!CharacterAdapter.select(state.characters, PLAYER_ID)) CharacterAdapter.create(state.characters, PLAYER_CHAR)
-
-    if (import.meta.env.DEV) {
-        Object.keys(AlchemyItems).forEach((key) => {
-            addItem(state, key, 20)
-        })
-    }
 
     return state
 }
